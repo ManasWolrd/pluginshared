d7893537a9a1622f2c72b0c7e83fd549e8c34df6
a5632645 <3022602213@qq.com>
2026-02-12 15:11:29 +0800

    a

--- preset_panel.hpp
+++ preset_panel.hpp
@@ -115,7 +115,7 @@ private:
 
             if (presetManager.GetUpdateData().HaveNewVersion()) {
                 menu.addItem("new version", []{
-                    juce::URL{UpdateData::GetPluginReleaseUrl()}.launchInDefaultBrowser();
+                    juce::URL{presetManager.GetUpdateData().GetPluginReleaseUrl()}.launchInDefaultBrowser();
                 });
             }
             else {
