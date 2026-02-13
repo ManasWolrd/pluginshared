0876ea30c5a2feaafa2f2dd9081e37d6e2a72090
a5632645 <3022602213@qq.com>
2026-02-13 20:31:10 +0800

    e

--- simd.hpp
+++ simd.hpp
@@ -36,6 +36,17 @@ static constexpr size_t LaneSize = sizeof(T) / sizeof(float);
 // extension?
 // ----------------------------------------
 
+template <class T, int... Indices>
+static inline T Shuffle(T a, T b) {
+#if defined(__clang__)
+    return __builtin_shufflevector(a, b, Indices...);
+#else
+    constexpr int count = sizeof...(Indices);
+    using MaskType = typename std::conditional<count == 4, Int128, Int256>::type;
+    return __builtin_shuffle(a, b, (MaskType){Indices...});
+#endif
+}
+
 static inline simde__m128 ToSimde(Float128 x) noexcept {
     return std::bit_cast<simde__m128>(x);
 }
@@ -75,7 +86,7 @@ static inline Float128 Frac128(Float128 x_) noexcept {
 #endif
 }
 static inline Float256 Frac256(Float256 x) noexcept {
-#ifdef SIMDE_X86_AVX2_NATIVE
+#ifdef SIMDE_X86_AVX_NATIVE
     auto x_ = ToSimde(x);
     simde__m256 i = simde_mm256_floor_ps(x_);
     auto s = simde_mm256_sub_ps(x_, i);
@@ -111,15 +122,15 @@ static inline Float256 combine(Float128 lo, Float128 hi) {
 }
 
 static inline std::array<Float128, 4> Transpose(Float128 x0, Float128 x1, Float128 x2, Float128 x3) noexcept {
-    Float128 tmp0 = __builtin_shufflevector(x0, x1, 0, 4, 1, 5); // row0[0], row1[0], row0[1], row1[1]
-    Float128 tmp1 = __builtin_shufflevector(x0, x1, 2, 6, 3, 7); // row0[2], row1[2], row0[3], row1[3]
-    Float128 tmp2 = __builtin_shufflevector(x2, x3, 0, 4, 1, 5); // row2[0], row3[0], row2[1], row3[1]
-    Float128 tmp3 = __builtin_shufflevector(x2, x3, 2, 6, 3, 7); // row2[2], row3[2], row2[3], row3[3]
+    Float128 tmp0 = Shuffle<Float128, 0, 4, 1, 5>(x0, x1); // row0[0], row1[0], row0[1], row1[1]
+    Float128 tmp1 = Shuffle<Float128, 2, 6, 3, 7>(x0, x1); // row0[2], row1[2], row0[3], row1[3]
+    Float128 tmp2 = Shuffle<Float128, 0, 4, 1, 5>(x2, x3); // row2[0], row3[0], row2[1], row3[1]
+    Float128 tmp3 = Shuffle<Float128, 2, 6, 3, 7>(x2, x3); // row2[2], row3[2], row2[3], row3[3]
 
-    Float128 row0 = __builtin_shufflevector(tmp0, tmp2, 0, 1, 4, 5); // [r0c0, r1c0, r2c0, r3c0]
-    Float128 row1 = __builtin_shufflevector(tmp0, tmp2, 2, 3, 6, 7); // [r0c1, r1c1, r2c1, r3c1]
-    Float128 row2 = __builtin_shufflevector(tmp1, tmp3, 0, 1, 4, 5); // [r0c2, r1c1, r2c2, r3c2]
-    Float128 row3 = __builtin_shufflevector(tmp1, tmp3, 2, 3, 6, 7); // [r0c3, r1c3, r2c3, r3c3]
+    Float128 row0 = Shuffle<Float128, 0, 1, 4, 5>(tmp0, tmp2); // [r0c0, r1c0, r2c0, r3c0]
+    Float128 row1 = Shuffle<Float128, 2, 3, 6, 7>(tmp0, tmp2); // [r0c1, r1c1, r2c1, r3c1]
+    Float128 row2 = Shuffle<Float128, 0, 1, 4, 5>(tmp1, tmp3); // [r0c2, r1c1, r2c2, r3c2]
+    Float128 row3 = Shuffle<Float128, 2, 3, 6, 7>(tmp1, tmp3); // [r0c3, r1c3, r2c3, r3c3]
     
     return {row0, row1, row2, row3};
 }
@@ -132,15 +143,15 @@ static inline std::array<Float256, 4> Transpose256(
     Float256 x2 = combine(c, g);
     Float256 x3 = combine(d, h);
 
-    Float256 m0 = __builtin_shufflevector(x0, x1, 0, 8, 1, 9, 4, 12, 5, 13);
-    Float256 m1 = __builtin_shufflevector(x0, x1, 2, 10, 3, 11, 6, 14, 7, 15);
-    Float256 m2 = __builtin_shufflevector(x2, x3, 0, 8, 1, 9, 4, 12, 5, 13);
-    Float256 m3 = __builtin_shufflevector(x2, x3, 2, 10, 3, 11, 6, 14, 7, 15);
+    Float256 m0 = Shuffle<Float256, 0, 8, 1, 9, 4, 12, 5, 13>(x0, x1);
+    Float256 m1 = Shuffle<Float256, 2, 10, 3, 11, 6, 14, 7, 15>(x0, x1);
+    Float256 m2 = Shuffle<Float256, 0, 8, 1, 9, 4, 12, 5, 13>(x2, x3);
+    Float256 m3 = Shuffle<Float256, 2, 10, 3, 11, 6, 14, 7, 15>(x2, x3);
 
-    Float256 out0 = __builtin_shufflevector(m0, m2, 0, 1, 8, 9, 4, 5, 12, 13);
-    Float256 out1 = __builtin_shufflevector(m0, m2, 2, 3, 10, 11, 6, 7, 14, 15);
-    Float256 out2 = __builtin_shufflevector(m1, m3, 0, 1, 8, 9, 4, 5, 12, 13);
-    Float256 out3 = __builtin_shufflevector(m1, m3, 2, 3, 10, 11, 6, 7, 14, 15);
+    Float256 out0 = Shuffle<Float256, 0, 1, 8, 9, 4, 5, 12, 13>(m0, m2);
+    Float256 out1 = Shuffle<Float256, 2, 3, 10, 11, 6, 7, 14, 15>(m0, m2);
+    Float256 out2 = Shuffle<Float256, 0, 1, 8, 9, 4, 5, 12, 13>(m1, m3);
+    Float256 out3 = Shuffle<Float256, 2, 3, 10, 11, 6, 7, 14, 15>(m1, m3);
 
     return {out0, out1, out2, out3};
 }
